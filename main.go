d1bb2de8c9f1c826fb27bc9d1da1c66b05acd0b5
micsama <1246659083@qq.com>
2021-02-24 16:25:08 +0800

    加入测试模式 2-24

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"io"
+	"os"
 	"sync"
 )
 
-const testing = true
+var testing bool
 
 var datebase = "data/date.db"
 var myurl string
@@ -18,6 +20,15 @@ var wg sync.WaitGroup  //sync
 var userdb *gorm.DB    //db
 
 func main() {
+	f, err := os.OpenFile("./config", os.O_RDONLY, 0600)
+	defer f.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		testing = false
+	} else {
+		_, _ = io.ReadAll(f)
+		testing = true
+	}
 	if testing {
 		myurl = "localhost"
 	} else {
@@ -32,8 +43,10 @@ func initsql() {
 	wg.Add(1)
 	var dberr error
 	if testing {
+		fmt.Println("testokookkkk")
 		userdb, dberr = gorm.Open("sqlite3", datebase)
 	} else {
+		fmt.Println("t----------111est")
 		userdb, dberr = gorm.Open("mysql", "sql_www.dzmfg:XaFrbPKEh65t6JGd@(127.0.0.1:3306)/sql_www.dzmfg?charset=utf8&parseTime=True&loc=Local")
 	}
 	if dberr != nil {
