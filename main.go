22b34e35bb5a7ae3e39c7fc984dc19a1ca310631
micsama <dzmfg@Mic.localdomain>
2021-03-05 11:09:42 +0800

    改成只用sqlite数据库了

--- main.go
+++ main.go
@@ -34,13 +34,14 @@ func main() {
 func initsql() {
 	wg.Add(1)
 	var dberr error
-	if testing {
-		fmt.Println("testokookkkk")
-		userdb, dberr = gorm.Open("sqlite3", database)
-	} else {
-		fmt.Println("t----------111est")
-		userdb, dberr = gorm.Open("mysql", "sql_www.dzmfg:XaFrbPKEh65t6JGd@(127.0.0.1:3306)/sql_www.dzmfg?charset=utf8&parseTime=True&loc=Local")
-	}
+	userdb, dberr = gorm.Open("sqlite3", database)
+	// if testing {
+	// 	fmt.Println("testokookkkk")
+	// 	userdb, dberr = gorm.Open("sqlite3", database)
+	// } else {
+	// 	fmt.Println("t----------111est")
+	// 	userdb, dberr = gorm.Open("mysql", "sql_www.dzmfg:XaFrbPKEh65t6JGd@(127.0.0.1:3306)/sql_www.dzmfg?charset=utf8&parseTime=True&loc=Local")
+	// }
 	if dberr != nil {
 		fmt.Println(dberr)
 	} else {
